bfed8c1c4ee0a7866e4398e9357ba675c56b0aa1
Antonio Andrade <antoniomaresmares001gmail.com>
2021-09-25 19:10:52 -0500

    add input

---
+++ src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; //peticiones http
+
+interface Coin{
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price:number
+  price_change_percentage_24h: number //valor de ultimas 24 horas
+  total_volume: number
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit {
+
+  coins:Coin[] = [];
+
+  //arreglo para hacer el refresh de la busqueda
+  filteredCoins:Coin[] = [];
+
+  titles: string[] = [
+    '#',
+    'Coin',
+    'Price',
+    'Price Change',
+    '24h Volume'
+  ];
+
+  searchText ='';
+
+  constructor( private http:HttpClient ){}
+
+  //Busqueda de la moneda
+  searchCoin(){
+    this.filteredCoins = this.coins.filter( (coin) =>
+      coin.name.toLowerCase().includes(this.searchText.toLowerCase()) ||
+      coin.symbol.toLowerCase().includes(this.searchText.toLowerCase()) 
+    );
+  }
+
+    
+    ngOnInit() {  
+      this.http
+        .get<Coin[]>(
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+          )
+          .subscribe( 
+            (res) =>{ 
+            console.log(res);
+            this.coins = res;
+            this.filteredCoins = res;  //Copia de datos filtrados
+          },
+          (err) => console.log(err)
+        ); 
+    }
+}
+
+
